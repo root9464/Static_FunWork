640680a4f90f62da188275bb05731b6b2d0a26a9
iO <senpai9464@yandex.ru>
2024-04-29 19:27:09 +0500

    +

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,20 @@
-document.getElementById('manbtn').addEventListener('click', function() {
+
+const fmn = document.getElementById('fmn');
+const fwm = document.getElementById('fwm');
+const label = document.createElement('label');
+const cityInput = document.querySelector('#city');
+const nameInput = document.querySelector('#name');
+const surnameInput = document.querySelector('#surname');
+const phoneInput = document.querySelector('#phone');
+const emailInput = document.querySelector('#email');
+const companyInput = document.querySelector('#company');
+const regionInput = document.querySelector('#region');
+const userInput = document.querySelector('#user');
+
+
+document.getElementById('manbtn').addEventListener('click', function () {
     document.querySelector('.form-mn').style.display = 'grid';
-    const formMan = document.querySelectorAll('.form-woomen');
+    const formMan = document.querySelectorAll('.form-role');
     formMan.forEach(function(element) {
         element.style.display = 'none';
     })
@@ -8,22 +22,69 @@ document.getElementById('manbtn').addEventListener('click', function() {
 
 document.getElementById('wombtn').addEventListener('click', function() {
     document.querySelector('.form-wm').style.display = 'grid';
-    const formWoomen = document.querySelectorAll('.form-woomen');
+    const formWoomen = document.querySelectorAll('.form-role');
     formWoomen.forEach(function(element) {
         element.style.display = 'none';
     });
 });
 
 
-const nameInput = document.querySelector('#name');
-const cityInput = document.querySelector('#city');
-const form = document.querySelector('.form');
-const label = document.createElement('label');
-const surnameInput = document.querySelector('#surname');
-const phoneInput = document.querySelector('#phone');
-const emailInput = document.querySelector('#email');
-const companyInput = document.querySelector('#company');
-const regionInput = document.querySelector('#region');
+document.getElementById('wm').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const name = nameInput.value.trim();
+    const city = cityInput.value.trim();
+
+    if (name!== '' && validateName(name)) nameInput.style.border = ''; else setError(nameInput, 'Введите корректный номер телефона или email');
+
+    if (city!== '') cityInput.style.border = ''; else setError(cityInput, 'Поле не может быть пустым');
+
+    if (name !== '' && city !== '') {
+        nameInput.style.border = '';
+        cityInput.style.border = '';
+        label.textContent = '';
+        fwm.removeChild(label);
+        console.log('Форма успешно валидирована');
+    };
+    
+});
+
+document.getElementById('mn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const name = userInput.value.trim();
+    const surname = surnameInput.value.trim();
+    const phone = phoneInput.value.trim();
+    const email = emailInput.value.trim();
+    const company = companyInput.value.trim();
+    const region = regionInput.value.trim();
+
+    if (name !== '') userInput.style.border = ''; else setError(userInput, 'Поле не может быть пустым');
+
+    if (surname !== '') surnameInput.style.border = ''; else setError(surnameInput, 'Поле не может быть пустым');
+
+    if (phone !== '') phoneInput.style.border = ''; else setError(phoneInput, 'Поле не может быть пустым');
+
+    if (email !== '') emailInput.style.border = ''; else setError(emailInput, 'Поле не может быть пустым');
+
+    if (company !== '') companyInput.style.border = ''; else setError(companyInput, 'Поле не может быть пустым');
+
+    if (region !== '') regionInput.style.border = ''; else setError(regionInput, 'Поле не может быть пустым');
+
+    if (name !== '' && surname !== '' && phone !== '' && email !== '' && company !== '' && region !== '') {
+
+        userInput.style.border = '';
+        surnameInput.style.border = '';
+        phoneInput.style.border = '';
+        emailInput.style.border = '';
+        companyInput.style.border = '';
+        regionInput.style.border = '';
+
+        label.textContent = '';
+        fmn.removeChild(label);
+        console.log('Форма успешно валидирована');
+    }
+})
 
 
 function validateName(name) {
@@ -32,39 +93,12 @@ function validateName(name) {
   return phoneRegex.test(name) || emailRegex.test(name);
 }
 
-document.getElementById('wm').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    const name = nameInput.value.trim();
-    const city = cityInput.value.trim();
-
-    switch (true) {
-        case name === '' && city === '':
-            setError(nameInput, 'Поле не может быть пустым');
-            setError(cityInput, 'Поле не может быть пустым');
-            break;
-        case !validateName(name):
-            setError(nameInput, 'Введите корректный номер телефона или email');
-            break;
-        case city === '':
-            setError(cityInput, 'Поле не может быть пустым');
-            break;
-        case name === '':
-            setError(nameInput, 'Поле не может быть пустым');
-            break;
-        default:
-            nameInput.style.border = '';
-            cityInput.style.border = '';
-            label.textContent = '';
-            form.removeChild(label);
-            console.log('Форма успешно валидирована');
-            break;
-    }
-});
 
 function setError(input, message) {
     input.style.border = '2px solid red';
     label.textContent = message;
     label.style.color = 'red';
-    form.appendChild(label);
+    input.parentNode.appendChild(label);
 }
+
+
